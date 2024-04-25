8230079b7e9dd50089af15b745f971953b436803
Juan Angela Alma <juanangelaalma@gmail.com>
2024-04-25 23:55:49 +0700

    Add Room on Seeder

--- src/seeders/20240423140310-demo-user.js
+++ src/seeders/20240423140310-demo-user.js
@@ -1,17 +1,26 @@
 'use strict';
 
+const RoomFactory = require('../factories/room.factory');
+const UserFactory = require('../factories/user.factory');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     const now = new Date();
-    await queryInterface.bulkInsert('Users', [{
+    const tenant = await UserFactory.createRandomUser({
       name: 'User Name',
       email: 'user@gmail.com',
       password: 'password',
       role: 'tenant',
-      createdAt: now,
-      updatedAt: now,
-    }], {});
+    })
+    const admin = await UserFactory.createRandomUser({
+      name: 'Admin Name',
+      email: 'admin@gmail.com',
+      password: 'password',
+      role: 'admin',
+    })
+
+    const room = await RoomFactory.createRoomUser({ user: tenant, code: 'K-101' })
   },
 
   async down(queryInterface, Sequelize) {
