08bb6d6d8dbd16bbdf727c68840478d8dac6ada6
Juan Angela Alma <juanangelaalma@gmail.com>
2024-04-29 00:08:08 +0700

    Add SSL to database config

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,13 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
   },
   test: {
     username: process.env.DB_USERNAME_TEST,
@@ -14,14 +20,26 @@ const config = {
     database: process.env.DB_DATABASE_TEST,
     host: process.env.DB_HOST_TEST,
     port: process.env.DB_PORT_TEST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
   }
 }
 
