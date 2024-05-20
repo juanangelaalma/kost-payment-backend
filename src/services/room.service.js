8c2396e43dd502dbb222a4b1d04c07541cf3982f
Juan Alma <juanangelaalma@gmail.com>
2024-05-20 12:42:18 +0700

    Fixed schedule when try to get room by startDate

--- src/services/room.service.js
+++ src/services/room.service.js
@@ -1,5 +1,5 @@
 const { Op } = require('sequelize')
-const { Room } = require('../models')
+const { Room, User } = require('../models')
 
 const getRoomByCode = async (code) => {
   return Room.findOne({
@@ -14,14 +14,26 @@ const createRoom = async (data) => {
 }
 
 const getRoomsStartingFromToday = async (currentDate) => {
-  return Room.findAll({
+  const users = await User.findAll({
     where: {
       startDate: {
         [Op.gte]: currentDate, // Membandingkan tanggal mulai dari hari ini
         [Op.lt]: new Date(currentDate.getTime() + 24 * 60 * 60 * 1000) // Kurang dari mulai hari besok
       }
-    }
+    },
+    include: [
+      {
+        model: Room,
+        as: 'room'
+      }
+    ]
   })
+
+  const rooms = users.map(user => {
+    return user.room
+  });
+
+  return rooms
 }
 
 const getAllRooms = async () => {
